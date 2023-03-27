82717d7abf00b01ab2a1ecd7580a503111f606b8
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-27 12:48:54 +0000

    intall and use of uuidv4

--- src/components/TodosLogic.jsx
+++ src/components/TodosLogic.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react';
+import { v4 as uuidv4 } from "uuid";
 import InputTodo from "@/components/InputTodo";
 import TodosList from '@/components/TodosList'
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -31,7 +32,7 @@ const TodosLogic = () => {
 
   const addTodoItem = (title) => {
     const newTodo = {
-      id: 4,
+      id: uuidv4(),
       title: title,
       completed: false,
     };
