2209c488ceb6b70afe52a502bb57148a088ec615
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-27 19:41:12 +0000

    create Navbar and Modal component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import '@/styles/app.css';
 
 import TodoApp from '@/components/TodoApp';
+import Navbar from "@/components/Navbar";
+import Modal from "@/components/Modal";
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
+    <Navbar />
+    <Modal />
     <TodoApp />
   </React.StrictMode>
 );
