8831f7b43cb31da2dd0771abadc4b8cc69a420a9
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-27 20:52:33 +0000

    remove nav and modal from the main file

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 import '@/styles/app.css';
 
 import TodoApp from '@/components/TodoApp';
-import Navbar from "@/components/Navbar";
-import Modal from "@/components/Modal";
 
 const domContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(domContainer);
 root.render(
   <React.StrictMode>
     <Router>
-      <Navbar />
-      <Modal />
       <TodoApp />
     </Router>
   </React.StrictMode>
